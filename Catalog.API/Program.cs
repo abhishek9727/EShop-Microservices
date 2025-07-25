bf7eeb7fba135960e70977daba2a68ac0187589a
ABhishek Meshram <abhishekmeshram44@gmail.com>
2025-07-25 19:18:14 +0530

    Add project files.

---
+++ Catalog.API/Program.cs
@@ -0,0 +1,24 @@
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddCarter();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
