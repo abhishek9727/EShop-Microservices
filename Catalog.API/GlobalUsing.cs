ec379d7fe62a2b9a14caa991f7c18799eba71c51
ABhishek Meshram <abhishekmeshram44@gmail.com>
2025-07-26 16:20:16 +0530

    Tilll now i am able to get value get all product is working get  product  by  Id is working get product  by Category is also working 75

--- Catalog.API/GlobalUsing.cs
+++ Catalog.API/GlobalUsing.cs
@@ -5,3 +5,4 @@ global using FluentValidation;
 global using Mapster;
 global using MediatR;
 global using Marten;
+global using Catalog.API.Exceptions;
---
+++ Catalog.API/Products/GetProductByCategroy/GetProductByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Products.GetProducts;
+using System.Threading;
+
+namespace Catalog.API.Products.GetProductByCategroy
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryHandler 
+        (IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+        : IQueryHandler <GetProductByCategoryQuery, GetProductByCategoryResult> 
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle Called With {@Query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
