9ec964d8cc015a60a7dba01b0cdaf5bd145bb3d8
ABhishek Meshram <abhishekmeshram44@gmail.com>
2025-07-30 13:24:39 +0530

    Added Basket Api now we will Move to our basket Api Branch

---
+++ Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+    public record DeleteBasketResult(bool IsSuccess);
+
+    public class DeleteBasetCommandValidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasetCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName Is Required");
+        }
+    }
+
+    public class DeleteBasketCommandHandler
+        : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            return new DeleteBasketResult(true);
+        }
+    }
+}
