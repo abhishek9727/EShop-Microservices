0d5117d65164b73a033634ff58ed044a3ececbf6
ABhishek Meshram <abhishekmeshram44@gmail.com>
2025-07-30 14:26:58 +0530

    Created basket api and test

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,9 +1,20 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
 //configure the http request pipeline
+app.MapCarter();
 
 app.Run();
